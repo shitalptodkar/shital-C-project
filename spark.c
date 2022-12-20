833af424c6f3f413a1de9460feb3b198dd1e0871
shitalptodkar <121082072+shitalptodkar@users.noreply.github.com>
2022-12-20 12:05:36 +0530

    upadate spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\n welcome");
+  printf("\n spark institute");
+  add();
+}
+void add()
+{
+  int a,b,c;
+  printf("\n enter a&b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n add=%d",c);
 }
